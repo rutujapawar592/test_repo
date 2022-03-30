5a97e85d0782b87bfd39ad19f3d644313bdb52f0
RP66148 <RP66148@POOLW42SLPC6454.ind.zensar.com>
2022-03-30 16:41:03 +0530

    Initial commit

--- eurekaserver/src/main/java/com/zensar/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/zensar/eurekaserver/EurekaserverApplication.java
@@ -3,7 +3,7 @@ package com.zensar.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//hello
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaserverApplication {
